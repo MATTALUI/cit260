5abd7d0ca847ea18568d1e6848450ccb8e3e48d5
Matthew Hummer <biggreyson@gmail.com>
2021-01-02 22:31:28 -0700

    initialize with first assignment

---
+++ W1dot1/src/io/mattalui/cit260/W1dot1.java
@@ -0,0 +1,11 @@
+package io.mattalui.cit260;
+
+public class W1dot1 {
+
+    public static void main(String[] args) {
+	    System.out.println("Matthew Hummer");
+        System.out.println("CIT260");
+        System.out.println("Fort Collins, Colorado");
+        System.out.println("Warm brownies with Chunky Monkey ice cream melting into it...");
+    }
+}
