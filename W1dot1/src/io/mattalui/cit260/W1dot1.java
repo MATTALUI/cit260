489f51f2236254c7b31fff7584e6737aaf8b18ef
Matthew Hummer <biggreyson@gmail.com>
2021-01-03 14:05:42 -0700

    add week 1 comments

--- W1dot1/src/io/mattalui/cit260/W1dot1.java
+++ W1dot1/src/io/mattalui/cit260/W1dot1.java
@@ -2,10 +2,14 @@ package io.mattalui.cit260;
 
 public class W1dot1 {
 
+    /**
+     * Write a Java program that displays your name, CIT260 and your section,
+     * your home town, and your favorite dessert.
+     */
     public static void main(String[] args) {
-        System.out.println("Matthew Hummer");
-        System.out.println("CIT260");
-        System.out.println("Fort Collins, Colorado");
-        System.out.println("Warm brownies with Chunky Monkey ice cream melting into it...");
+        System.out.println("Matthew Hummer"); // My Name
+        System.out.println("CIT260"); // My Class
+        System.out.println("Fort Collins, Colorado"); // My Hometown
+        System.out.println("Warm brownies with Chunky Monkey ice cream melting into it..."); // My Favourite Dessert
     }
 }
--- W1dot2/src/io/mattalui/cit260/W1dot2.java
+++ W1dot2/src/io/mattalui/cit260/W1dot2.java
@@ -1,7 +1,9 @@
 package io.mattalui.cit260;
 
 public class W1dot2 {
-
+    /**
+     * Write a short Java program that outputs the following pattern.
+     */
     public static void main(String[] args) {
         System.out.println("     J     A    V     V     A");
         System.out.println("     J    A A    V   V     A A");
