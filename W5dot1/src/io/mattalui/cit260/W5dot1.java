0cd2db71b6f3a2c324169882baf77e8c82d83d29
Matthew Hummer <biggreyson@gmail.com>
2021-01-15 21:38:43 -0700

    5.1

---
+++ W5dot1/src/io/mattalui/cit260/W5dot1.java
@@ -0,0 +1,45 @@
+package io.mattalui.cit260;
+
+public class W5dot1 {
+    /**
+     * Write a short Java program that does the following:
+     *
+     * 1) Tells the user what the program does.
+     * 2) Uses a loop to calculate pounds per kilogram (1 kilogram = 2.2 pounds) and display the following table.
+     * 3) Outputs a goodbye message.
+     * @param args
+     */
+    public static void main(String[] args) {
+        displayIntro();
+	    displayConversions();
+        sayGoodBye();
+    }
+
+    /**
+     * Displays an introduction
+     */
+    public static void displayIntro(){
+        System.out.println("This Program displays a table of kilogram to punds conversions.\n");
+    }
+
+    /**
+     * Displays pounds to kilograms tables for 1-15 kilograms
+     */
+    public static void displayConversions() {
+        System.out.println("kilograms\tpounds");
+        System.out.println("---------\t------");
+        int kiloLimit = 15; // Only expecte 1-15 in output
+        for (int kilo = 1; kilo <= kiloLimit; kilo++){
+            float lbsPerKilo = 2.2f; // 1 kilogram = 2.2 pound
+            float lbs = kilo * lbsPerKilo;
+            System.out.format("%d\t\t\t%.1f\n", kilo, lbs);
+        }
+    }
+
+    /**
+     * I don't know why you say, "Goodbye", I say, "Hello"
+     */
+    public static void sayGoodBye(){
+        System.out.println("\nGoodbye.");
+    }
+}
